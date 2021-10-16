6ce03136c8a657f2b8d07c41cebf4a805fa03c1d
lucaslgr <lucaslgr1206@gmail.com>
2021-10-16 14:17:33 -0300

    Implementa aula 7, um pouco sobre strings e Regex

---
+++ modulo-java/aula7-app-java-console-str-regex/src/A_vazio/Programa.java
@@ -0,0 +1,15 @@
+package A_vazio;
+
+public class Programa {
+    public static void main(String[] args) {
+        //empty
+        System.out.println(Caractere.metodoDigitado(""));
+
+        //null
+        System.out.println(Caractere.metodoDigitado(null));
+
+
+        //null
+        System.out.println(Caractere.metodoDigitado("123eafrvb"));
+    }
+}
---
+++ modulo-java/aula7-app-java-console-str-regex/src/B_cnpf/Programa.java
@@ -0,0 +1,9 @@
+package B_cnpf;
+
+public class Programa {
+    public static void main(String[] args) {
+        System.out.println(Regex.adicionarMascara("1234567890123"));
+
+        System.out.println(Regex.adicionarMascara("93897562000160"));
+    }
+}
